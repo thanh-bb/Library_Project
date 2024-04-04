66d7a6b9f07e2e870a9163cec5333448f758675c
thanh-bb <carrot1420@gmail.com>
2024-04-04 20:48:13 +0700

    crud NXB

--- Library_API/Controllers/NhaXuatBanController.cs
+++ Library_API/Controllers/NhaXuatBanController.cs
@@ -41,5 +41,85 @@ namespace Library_API.Controllers
 
             return new JsonResult(table);
         }
+
+
+        [HttpPost]
+        public JsonResult Post(NhaXuatBan nxb)
+        {
+            string query = @"insert into dbo.NhaXuatBan (nxb_TenNhaXuatBan)
+                            values (@nxb_TenNhaXuatBan)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            { 
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@nxb_TenNhaXuatBan", nxb.NxbTenNhaXuatBan);
+                    myCommand.ExecuteNonQuery();
+                }
+                myCon.Close();
+            }
+            return new JsonResult("Thêm thành công");
+
+        }
+
+
+        [HttpPut]
+        public JsonResult Put (NhaXuatBan nxb)
+        {
+            string query = @"Update dbo.NhaXuatBan
+                        set nxb_TenNhaXuatBan = @nxb_TenNhaXuatBan
+                        where nxb_Id = @nxb_Id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@nxb_Id", nxb.NxbId);
+                    myCommand.Parameters.AddWithValue("@nxb_TenNhaXuatBan", nxb.NxbTenNhaXuatBan);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Cập nhật thành công");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete ( int id)
+        {
+            string query = @"delete from dbo.NhaXuatBan where nxb_Id = @nxb_Id";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("MyConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@nxb_Id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Xoa thanh cong");
+        }
+
+
+
     }
 }
